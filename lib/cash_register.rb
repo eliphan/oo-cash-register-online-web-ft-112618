a3523f3140aed791d81be887e16753a4025b299a
Elise Phan <aliceconnic@gmail.com>
2018-12-07 07:57:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+  attr_accessor :total, :discount, :item_name
+  
+  def initialize(discount = nil, total = 0)
+    @discount = discount
+    @total = total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total = total + price * quantity
+  end
+  
+  def apply_discount(total, discount)
+    @total = total.to_f / discount.to_f * 100
+  end
+  
+    
+ def items
+    items = []
+    @item_name = item_name
+    items << @item_name
+ end  
+ 
+ def void_last_transaction
+   @total = total - @total
+ end
+  
+end
 
